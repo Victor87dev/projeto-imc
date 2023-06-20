30fd01655b5cc8ed9cfd10118b99afaa88818c94
João Victor <devsjoaovvictor@gmail.com>
2023-06-20 11:33:36 -0300

    tabela e imc pronto

--- forms.js
+++ forms.js
@@ -1,13 +1,7 @@
 const caixaPrincipal = document.getElementById("caixa_principal")
 const botao = document.getElementById("botao")
-const menor_18 = document.getElementById("menor18")
-const entre18_24 = document.getElementById("entre18-24")
-const entre25_29 = document.getElementById("entre25-29")
-const entre30_39 = document.getElementById("entre30-39")
-const maior_40 = document.getElementById("maior40")
 
 let verifica = false 
-let selecionado = false
 
 botao.addEventListener("click",(evt)=>{
 let inputP = document.getElementById("input_peso").value 
@@ -31,62 +25,42 @@ if(!verifica){
   div.innerHTML += `IMC: ${resultado}`
 }
 
-funcaoSelecionar(inputPeso,inputAltura)
+caixaSelecionada(inputPeso,inputAltura)
 
 })
 
-const funcaoSelecionar = (p,a)=>{
-  
+const caixaSelecionada = (p,a)=>{
   const resultadoAltura = (a * a)
   const resultadoFinal = (p / resultadoAltura).toFixed(2)
 
-  if(!selecionado){
-    selecionado = true 
-    if(resultadoFinal < 18.5){
-     menor_18.setAttribute("class","resultadoSelecionado")
-    }else if(resultadoFinal >= 18.5 && resultadoFinal <= 24.9) {
-     entre18_24.setAttribute("class","resultadoSelecionado")
-    }else if(resultadoFinal > 24.9 && resultadoFinal <= 29.9){ 
-     entre25_29.setAttribute("class","resultadoSelecionado")
-    }else if(resultadoFinal > 29.9 && resultadoFinal <= 39.9){
-     entre30_39.setAttribute("class","resultadoSelecionado")
-    }else{ 
-     maior_40.setAttribute("class","resultadoSelecionado")
-    }
+  const arrayTabelas = [...document.querySelectorAll(".itensTabela")]
+      
+  if(resultadoFinal < 18.5){
+    arrayTabelas.map((el)=>{
+      el.classList.remove("class","resultadoSelecionado")
+      arrayTabelas[0].classList.add("class","resultadoSelecionado")
+    })
+  }else if(resultadoFinal >= 18.5 && resultadoFinal <= 24.9){
+   for(let i = 0;i <= arrayTabelas.length;i++){
+    arrayTabelas[i].classList.remove("class","resultadoSelecionado")
+    arrayTabelas[1].classList.add("class","resultadoSelecionado")
+   }   
+  }else if(resultadoFinal > 24.9 && resultadoFinal <= 29.9){    
+   for(let i = 0;i <= arrayTabelas.length;i++){
+    arrayTabelas[i].classList.remove("class","resultadoSelecionado")
+    arrayTabelas[2].classList.add("class","resultadoSelecionado")
+   }   
+  }else if(resultadoFinal > 29.9 && resultadoFinal <= 39.9){    
+    for(let i = 0;i <= arrayTabelas.length;i++){
+     arrayTabelas[i].classList.remove("class","resultadoSelecionado")
+     arrayTabelas[3].classList.add("class","resultadoSelecionado")
+    }    
   }else{
-    if(resultadoFinal < 18.5){
-      menor_18.setAttribute("class","resultadoSelecionado")
-      entre18_24.classList.remove("class","resultadoSelecionado")
-      entre25_29.classList.remove("class","resultadoSelecionado")
-      entre30_39.classList.remove("class","resultadoSelecionado")
-      maior_40.classList.remove("class","resultadoSelecionado")
-    }else if(resultadoFinal >= 18.5 && resultadoFinal <= 24.9) {
-      entre18_24.setAttribute("class","resultadoSelecionado")
-      menor_18.classList.remove("class","resultadoSelecionado")
-      entre25_29.classList.remove("class","resultadoSelecionado")
-      entre30_39.classList.remove("class","resultadoSelecionado")
-      maior_40.classList.remove("class","resultadoSelecionado")
-     }else if(resultadoFinal > 24.9 && resultadoFinal <= 29.9){
-      entre25_29.setAttribute("class","resultadoSelecionado")
-      menor_18.classList.remove("class","resultadoSelecionado")
-      entre18_24.classList.remove("class","resultadoSelecionado")
-      entre30_39.classList.remove("class","resultadoSelecionado")
-      maior_40.classList.remove("class","resultadoSelecionado")
-     }else if(resultadoFinal > 29.9 && resultadoFinal <= 39.9){
-      entre30_39.setAttribute("class","resultadoSelecionado")
-      menor_18.classList.remove("class","resultadoSelecionado")
-      entre18_24.classList.remove("class","resultadoSelecionado")
-      entre25_29.classList.remove("class","resultadoSelecionado")
-      maior_40.classList.remove("class","resultadoSelecionado")
-     }else{
-      maior_40.setAttribute("class","resultadoSelecionado")
-      menor_18.classList.remove("class","resultadoSelecionado")
-      entre18_24.classList.remove("class","resultadoSelecionado")
-      entre25_29.classList.remove("class","resultadoSelecionado")
-      entre30_39.classList.remove("class","resultadoSelecionado") 
-     }
-  } 
+    for(let i = 0;i <= arrayTabelas.length;i++){
+      arrayTabelas[i].classList.remove("class","resultadoSelecionado")
+      arrayTabelas[4].classList.add("class","resultadoSelecionado")
+     }    
+  }
 
 }
-
 
